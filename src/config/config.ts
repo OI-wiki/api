b58e3b66b0355d496c0d8566fde71e6ed79f3b10
Margatroid <i@margatroid.xyz>
2019-08-27 19:48:23 +0800

    chore: 统一代码风格, 设置 http statuscode
    去除了所有分号, 将所有双引号替换为单引号, 去除了重复的 status code, 将 base folder 设置为默认从环境变量读取
    
    BREAKING CHANGE: wiki 和 wikiraw  的 response 中不再存在 status 字段

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,13 @@
 import { sync } from 'glob'
 import { union } from 'lodash'
+import { env } from 'process'
 
 export default class Config {
   public static port: number = 3000
   public static routes: string = './dist/routes/**/*.js'
   public static models: string = './dist/models/**/*.js'
   public static useMongo: boolean = false
-  public static baseFolder: string = '/home/ir1d/Documents/repo/OI-wiki'
+  public static baseFolder: string = env.OI_WIKI_PATH || '/home/ir1d/Documents/repo/OI-wiki'
   public static mongodb =
     process.env.NODE_ENV === 'docker'
       ? 'mongodb://mongo:27017/express-typescript-starter'
--- src/controllers/wiki.server.controller.ts
+++ src/controllers/wiki.server.controller.ts
@@ -1,44 +1,45 @@
-import { Request, Response } from "express";
-// import { inspect } from "util";
-import Config from "../config/config";
-import * as path from "path";
-import * as fs from "fs";
-import * as _ from "lodash";
+import { Request, Response } from 'express'
+import Config from '../config/config'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as _ from 'lodash'
 
 export default class IndexController {
   public index(req: Request, res: Response, next: Function): void {
-    res.render("index", { title: "Express" });
+    res.render('index', { title: 'Express' })
   }
 
   public wiki(req: Request, res: Response): void {
     try {
-      let requested_path = req.params["0"];
-      requested_path = _.trim(requested_path, "/");
+      let requested_path = req.params['0']
+      requested_path = _.trim(requested_path, '/')
       let file_path =
-        path.join(Config.baseFolder, "docs", requested_path) + ".md";
-      let file_ = fs.readFileSync(file_path, "utf8");
-      let result = file_;
-      res.send({ status: 1, results: result });
+        path.join(Config.baseFolder, 'docs', requested_path) + '.md'
+      let file_ = fs.readFileSync(file_path, 'utf8')
+      let result = file_
+      res.send({ results: result })
     } catch (e) {
-      console.log(e);
-      res.send({ status: 0 });
+      console.error(e)
+      res.statusCode = 404
+      res.send({ message: 'page node found' })
     }
   }
   public raw(req: Request, res: Response): void {
     try {
-      let requested_path = req.params["0"];
-      requested_path = _.trim(requested_path, "/");
+      let requested_path = req.params['0']
+      requested_path = _.trim(requested_path, '/')
       let file_path =
-        path.join(Config.baseFolder, "docs", requested_path) + ".md";
-      let file_ = fs.readFileSync(file_path, "utf8");
-      let result = file_;
-      res.setHeader("Content-Type", "text/plain");
-      res.send(result);
+        path.join(Config.baseFolder, 'docs', requested_path) + '.md'
+      let file_ = fs.readFileSync(file_path, 'utf8')
+      let result = file_
+      res.setHeader('Content-Type', 'text/plain')
+      res.send(result)
     } catch (e) {
-      console.log(e);
-      res.send({ status: 0 });
+      console.error(e)
+      res.statusCode = 404
+      res.send({ message: 'page node found' })
     }
   }
 }
 
-export const wikiController = new IndexController();
+export const wikiController = new IndexController()
