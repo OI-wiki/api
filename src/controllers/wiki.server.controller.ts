cbdd0c6e9e5874c62ff7c097fae19c27f4bb320e
Margatroid <i@margatroid.xyz>
2019-08-27 19:57:54 +0800

    feat: 异步读取文件, 并将 ts-node 添加到 dev-dependency 中
    ts-node 在测试的时候被使用了, 但是没有被添加进依赖

--- src/controllers/wiki.server.controller.ts
+++ src/controllers/wiki.server.controller.ts
@@ -3,41 +3,42 @@ import Config from '../config/config'
 import * as path from 'path'
 import * as fs from 'fs'
 import * as _ from 'lodash'
+import { promisify } from 'util'
+
+const readFileAsync = promisify(fs.readFile)
 
 export default class IndexController {
   public index(req: Request, res: Response, next: Function): void {
     res.render('index', { title: 'Express' })
   }
 
-  public wiki(req: Request, res: Response): void {
+  public async wiki(req: Request, res: Response): Promise<void> {
     try {
       let requested_path = req.params['0']
       requested_path = _.trim(requested_path, '/')
       let file_path =
         path.join(Config.baseFolder, 'docs', requested_path) + '.md'
-      let file_ = fs.readFileSync(file_path, 'utf8')
-      let result = file_
+      let result = await readFileAsync(file_path, 'utf8')
       res.send({ results: result })
     } catch (e) {
       console.error(e)
       res.statusCode = 404
-      res.send({ message: 'page node found' })
+      res.send({ message: 'page not found' })
     }
   }
-  public raw(req: Request, res: Response): void {
+  public async raw(req: Request, res: Response): Promise<void> {
     try {
       let requested_path = req.params['0']
       requested_path = _.trim(requested_path, '/')
       let file_path =
         path.join(Config.baseFolder, 'docs', requested_path) + '.md'
-      let file_ = fs.readFileSync(file_path, 'utf8')
-      let result = file_
+      let result = await readFileAsync(file_path, 'utf8')
       res.setHeader('Content-Type', 'text/plain')
       res.send(result)
     } catch (e) {
       console.error(e)
       res.statusCode = 404
-      res.send({ message: 'page node found' })
+      res.send({ message: 'page not found' })
     }
   }
 }
